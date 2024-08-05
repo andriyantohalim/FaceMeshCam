37a251144f09f33b6ce4baaca083394d47919f26
Andriyanto Halim <andriyanto.halim@gmail.com>
2024-08-05 21:41:00 +0800

    Initial Commit

---
+++ FaceMeshCam/FaceMeshCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMeshCamApp.swift
+//  FaceMeshCam
+//
+//  Created by Andriyanto Halim on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceMeshCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
